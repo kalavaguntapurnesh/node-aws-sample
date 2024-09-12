011d402bc420c2092550f504265ef3c36b77db37
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-09-12 11:27:54 +0530

    Version 7.0

--- users.js
+++ users.js
@@ -5,7 +5,7 @@ exports.users = [
     picture: "http://placehold.it/32x32",
     age: 22,
     eyeColor: "green",
-    name: "Virat Kohli",
+    name: "Sri Krishna",
     gender: "male",
     email: "hardingtaylor@lexicondo.com",
     phone: "+1 (839) 440-2917",
